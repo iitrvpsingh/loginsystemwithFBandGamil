3d4d623f475ef8529fa36b7df45871669ebe372b
vishwanath <vishwanath.pratap91@gmail.com>
2018-05-24 00:40:22 +0530

    Base

---
+++ src/main/java/com/test/loginwithsocialmedia/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.test.loginwithsocialmedia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/wel",method=RequestMethod.GET)
+	@ResponseBody
+	public String tets() {
+		return "hello";
+	}
+}
